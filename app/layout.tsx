efa8e4d9ad6b320d420d84d61b0eaa197eddc38b
vatsan-0613 <srivatsan050@gmail.com>
2024-05-13 16:12:54 +0530

    updated favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,16 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import favicon from './favicon.png'
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "GenAI",
   description: "Transform Ideas into apps  with AI",
+  icons : {
+    icon : '/favicon.png'
+  }
 };
 
 export default function RootLayout({
@@ -16,7 +20,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}</body>
     </html>
   );
 }
