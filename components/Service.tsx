076a4f299c0241eff38d3af3e47ad2d7eeff6599
vatsan-0613 <srivatsan050@gmail.com>
2024-05-13 15:24:31 +0530

    tweaked responsiveness

--- components/Service.tsx
+++ components/Service.tsx
@@ -13,8 +13,8 @@ const Service: React.FC<ServiceProps> = ({ flex, phoneImg }) => {
     return (
         <section className={`${flex} max-sm:flex-col-reverse items-center flex mt-16 w-full px-10 justify-between mb-16`}>
             <div className='sm:w-[50%] text-center'>
-                <h2 className='sm:text-5xl text-3xl font-semibold mb-5 sm:leading-tight mt-5 sm:mt-0 text-center sm:text-start'><span className='text-buttonOrange'>Unleash</span> the power of AI with GenAI</h2>
-                <p className='text-smallText sm-leading-8 leading-6 sm:text-lg text-sm text-center sm:text-start'>
+                <h2 className='md:text-5xl  sm:text-3xl text-2xl font-semibold mb-5 sm:leading-tight mt-5 sm:mt-0 text-center sm:text-start'><span className='text-buttonOrange'>Unleash</span> the power of AI with GenAI</h2>
+                <p className='text-smallText sm-leading-8 leading-6 sm:text-md text-sm text-center sm:text-start'>
                     Stay ahead of the curve with our Dynamic Trend Analysis feature.
                     Unlock the full potential of your tech stack with Horizon&apos;s intuitive
                     and Stay ahead of the curve with our Dynamic Trend Analysis feature.
